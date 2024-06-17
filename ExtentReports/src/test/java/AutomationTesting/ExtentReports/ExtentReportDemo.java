5b5ec390f816fab199fe12627c236f9a5d4c8544
utkarshducoder <83503804+utkarshducoder@users.noreply.github.com>
2024-06-17 19:06:41 +0530

    Add files via upload

---
+++ ExtentReports/src/test/java/AutomationTesting/ExtentReports/ExtentReportDemo.java
@@ -0,0 +1,35 @@
+package AutomationTesting.ExtentReports;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReportDemo {
+	ExtentReports extent;
+	@BeforeTest
+	public void config() {
+		String path = System.getProperty("user.dir")+"\\reports\\index.html";
+		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+		reporter.config().setReportName("Web Automation Results");
+		reporter.config().setDocumentTitle("Test Results");
+		
+		extent = new ExtentReports();
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("Tester", "Utkarsh Gupta");
+	}
+	
+	
+	@Test
+	public void initialDemo() {
+		extent.createTest("Initial Demo");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/");
+		System.out.println(driver.getTitle());
+		extent.flush();
+	}
+	
+}
